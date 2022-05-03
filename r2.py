e723873f62ec342207fe651ad9090064e3ae375a
Aurelia <109011218@live.asia.edu.tw>
2022-05-03 20:53:54 +0800

    add new file

--- r2.py
+++ r2.py
@@ -2,11 +2,11 @@
 data = []
 count = 0
 with open('original.txt','r') as f:
-	 for line in f:
-	 	 data.append(line)
-	 	 count = count + 1
-	 	 if count % 1000 == 0:
-                  print(len(data))
+	for line in f:
+		data.append(line)
+		count = count + 1
+		if count % 1000 == 0:
+			    print(len(data))
 print('檔案讀取完了','總共有',len(data),'筆資料')
 
 sum_len = 0
@@ -14,5 +14,17 @@ for d in data:
 	sum_len = sum_len + len(d)
 print('平均留言長度',sum_len/len(data))
 
+#篩選留言<100
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有',len(new),'留言長度<100')
+print(new[0])
 
-
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有',len(good),'留言提到goog')
+print(good[0])
